fef566783c0c187aee02b305f38765892c734ba5
nousantx <nousantx@gmail.com>
2024-07-15 08:09:25 +0700

    feat: enhance custom value feature

--- src/ts/tenoxui.ts
+++ src/ts/tenoxui.ts
@@ -1,5 +1,5 @@
 /*!
- * tenoxui/core v1.0.0-alpha.1
+ * tenoxui/core v1.0.0-alpha.3
  * Licensed under MIT (https://github.com/tenoxui/css/blob/main/LICENSE)
  */
 
@@ -39,10 +39,16 @@ class makeTenoxUI {
   private valueHandler(type: string, value: string, unit: string): string {
     // use `values` from `valueRegistry` if match
     const registryValue = this.valueRegistry[value] as string;
+    const properties = this.styleAttribute[type];
     // use `values` from registry or default value
     let resolvedValue = registryValue || value;
+
+    // If no value is provided, and properties has a predefined value, use it
+    if (typeof properties === "object" && "value" in properties && !properties.value.includes("{value}")) {
+      return properties.value;
+    }
     // css variable classname, started with `$` and the value after it will treated as css variable
-    if (resolvedValue.startsWith("$")) {
+    else if (resolvedValue.startsWith("$")) {
       return `var(--${resolvedValue.slice(1)})`;
     }
     // custom value handler
@@ -112,27 +118,7 @@ class makeTenoxUI {
       }
     });
   }
-  // responsive handlet
-  private handleResponsive(breakpointPrefix: string, type: string, value: string, unit: string): void {
-    const handleResize = () => {
-      // get screen size
-      const windowWidth = window.innerWidth;
-      // creating matchpoint to apply the styles
-      const matchPoint = this.breakpoints.find(bp => this.matchBreakpoint(bp, breakpointPrefix, windowWidth));
-      // if match?
-      if (matchPoint) {
-        // apply the style when the breakpoints is macth with the screen size
-        this.addStyle(type, value, unit);
-      } else {
-        // remove value :)
-        this.htmlElement.style[type] = "";
-      }
-    };
-    // start the resize handler
-    handleResize();
-    // apply when the screen size is changing (maybe caused the memory leaks)
-    window.addEventListener("resize", handleResize);
-  }
+
   // match point
   private matchBreakpoint(bp: Breakpoint, prefix: string, width: number): boolean {
     // don't ask me... :(
@@ -150,16 +136,35 @@ class makeTenoxUI {
     return str.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
     // reason? The pseudo handler not working properly whenever the property defined with `camelCase`
   }
-  // pseudo selector imitation
-  private pseudoHandler(type: string, value: string, unit: string, pseudoEvent: string, revertEvent: string): void {
-    // get the
-    const propsName = this.getPropName(type);
-    const styleInitValue = this.getInitialValue(propsName);
-    const applyStyle = () => this.addStyle(type, value, unit);
-    const revertStyle = () => this.revertStyle(propsName, styleInitValue);
-    this.htmlElement.addEventListener(pseudoEvent, applyStyle);
-    this.htmlElement.addEventListener(revertEvent, revertStyle);
+  // responsive className handler
+  private handleResponsive(breakpointPrefix: string, type: string, value: string, unit: string): void {
+    const properties = this.styleAttribute[type];
+
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      const matchPoint = this.breakpoints.find(bp => this.matchBreakpoint(bp, breakpointPrefix, windowWidth));
+
+      // apply exact styles if matches
+      if (matchPoint) {
+        // object `type` style
+        if (this.isObjectWithValue(properties)) {
+          this.addStyle(type);
+        } else {
+          // apply regular style
+          this.addStyle(type, value, unit);
+        }
+      } else {
+        // remove value
+        this.htmlElement.style[type as any] = "";
+      }
+    };
+
+    // start the resize handler
+    handleResize();
+    // apply when the screen size is changing
+    window.addEventListener("resize", handleResize);
   }
+
   // utility to get the type from the property's name
   private getPropName(type: string): string | string[] {
     if (type.startsWith("[--") && type.endsWith("]")) {
@@ -183,75 +188,174 @@ class makeTenoxUI {
     }
     return this.htmlElement.style.getPropertyValue(propsName);
   }
+
+  private pseudoHandler(type: string, value: string, unit: string, pseudoEvent: string, revertEvent: string): void {
+    // the type's name inside of property
+    const properties = this.styleAttribute[type];
+    // get the property's name
+    const propsName = this.getPropName(type);
+    // get initial style
+    const styleInitValue = this.getInitialValue(propsName);
+
+    // applyStyle logic
+    const applyStyle = () => {
+      // is the propeties an object?
+      if (this.isObjectWithValue(properties)) {
+        // if has "{value}", use regular styling method
+        if (properties.value.includes("{value}")) {
+          this.addStyle(type, value, unit);
+        } else {
+          // if doesn't have any "{value}", get only the type, as className
+          this.addStyle(type);
+        }
+      } else {
+        // else, use default styling
+        this.addStyle(type, value, unit);
+      }
+    };
+
+    // revert style helper
+    const revertStyle = () => this.revertStyle(propsName, styleInitValue);
+
+    // add the listener
+    this.htmlElement.addEventListener(pseudoEvent, applyStyle);
+    this.htmlElement.addEventListener(revertEvent, revertStyle);
+  }
+
   // revert value when the listener is done
   private revertStyle(propsName: string | string[], styleInitValue: { [key: string]: string } | string): void {
+    // if the property is defined as an object / multiple properties
     if (Array.isArray(propsName)) {
       propsName.forEach(propName => {
         this.setCssVar(propName, (styleInitValue as { [key: string]: string })[propName]);
       });
-    } else {
+    }
+    // single property
+    else {
       this.setCssVar(propsName, styleInitValue as string);
     }
   }
-  // element's classnames parser
-  private parseClassName(className: string): [string | undefined, string, string, string] | null {
-    // use `regexp` to match all possible classmame
-    const match = className.match(
-      /(?:([a-zA-Z0-9-]+):)?(-?[a-zA-Z0-9_]+(?:-[a-zA-Z0-9_]+)*|\[--[a-zA-Z0-9_-]+\])-(-?(?:\d+(\.\d+)?)|(?:[a-zA-Z0-9_]+(?:-[a-zA-Z0-9_]+)*(?:-[a-zA-Z0-9_]+)*)|(?:#[0-9a-fA-F]+)|(?:\[[^\]]+\])|(?:\$[^\s]+))([a-zA-Z%]*)/
-    );
-    if (!match) return null;
-    // get prefix, type, value, and the unit from match
-    const [, prefix, type, value, , unit] = match;
-    return [prefix, type, value, unit];
-  }
+
   // main styler, handling the type, property, and value
-  public addStyle(type: string, value: string, unit: string): void {
+  public addStyle(type: string, value?: string, unit?: string): void {
     // get css property from styleAttribute
     const properties = this.styleAttribute[type];
+
+    // if no value is provided and properties is an object with a 'value' key, use that value
+    if (!value && this.isObjectWithValue(properties)) {
+      value = properties.value;
+    }
+
+    // don't process type with no value
+    if (!value) return;
+
     // compute values
-    let resolvedValue = this.valueHandler(type, value, unit);
+    let resolvedValue = this.valueHandler(type, value, unit || "");
+
     // logic to apply the styles
-    // css variable type handler
+    // css variable className
     if (type.startsWith("[--") && type.endsWith("]")) {
       this.setCssVar(type.slice(1, -1), resolvedValue);
     }
-    // custom value of type and property handler
+    // custom value handler
     else if (typeof properties === "object" && "property" in properties) {
       this.setCustomValue(properties as { property: string | string[]; value?: string }, resolvedValue);
     }
-    // regular classname handler / default type and property
+    // regular/default value handler
     else if (properties) {
       this.setDefaultValue(properties as string | string[], resolvedValue);
     }
   }
   // function to match the classnames with the correct handler
+  private parseClassName(
+    className: string
+  ): [string | undefined, string, string | undefined, string | undefined] | null {
+    // using regexp to parse all possible class names
+    const match = className.match(
+      /(?:([a-zA-Z0-9-]+):)?(-?[a-zA-Z0-9_]+(?:-[a-zA-Z0-9_]+)*|\[--[a-zA-Z0-9_-]+\])-(-?(?:\d+(\.\d+)?)|(?:[a-zA-Z0-9_]+(?:-[a-zA-Z0-9_]+)*(?:-[a-zA-Z0-9_]+)*)|(?:#[0-9a-fA-F]+)|(?:\[[^\]]+\])|(?:\$[^\s]+))([a-zA-Z%]*)/
+    );
+
+    // don't do anything if the class name is didn't match, maybe that is class name from outside tenoxui environment
+    if (!match) return null;
+
+    // returning parsed class name
+    const [, prefix, type, value, , unit] = match;
+    return [prefix, type, value, unit];
+  }
+
+  // get value from custom value
+  private isObjectWithValue(typeAttribute: any): typeAttribute is { property: string | string[]; value: string } {
+    return (
+      // is the styleAttribute[type] is an object
+      typeof typeAttribute === "object" &&
+      // the styleAttribute[type] must have a value
+      typeAttribute !== null &&
+      // value in styleAttribute[type]
+      "value" in typeAttribute &&
+      // property in styleAttribute[type]
+      "property" in typeAttribute
+    );
+  }
+
+  // class name handler
   public applyStyles(className: string): void {
-    // get the parsed classname
-    const parts = this.parseClassName(className);
-    if (!parts) return;
-    // matching the parts, like type and value
-    const [prefix, type, value, unit] = parts;
-    // if classmame has prefix
-    if (prefix) {
-      switch (prefix) {
-        // hover prefix handler
-        case "hover":
-          this.pseudoHandler(type, value, unit, "mouseover", "mouseout");
-          break;
-        // focus prefix state handler
-        case "focus":
-          this.pseudoHandler(type, value, unit, "focus", "blur");
-          break;
-        // responsive breakpoint prefix
-        default:
-          this.handleResponsive(prefix, type, value, unit);
+    // check for prefix
+    const [prefix, type] = className.split(":");
+    const getType = type || prefix;
+    const getPrefix = type ? prefix : undefined;
+
+    // check if the type directly matches a property with a predefined value
+    if (this.styleAttribute[getType] && this.isObjectWithValue(this.styleAttribute[getType])) {
+      const properties = this.styleAttribute[getType];
+
+      if (this.isObjectWithValue(properties)) {
+        const value = properties.value;
+
+        // if the classname has prefix
+        if (getPrefix) {
+          switch (getPrefix) {
+            // hover prefix
+            case "hover":
+              this.pseudoHandler(getType, value, "", "mouseover", "mouseout");
+              break;
+            // focus prefix
+            case "focus":
+              this.pseudoHandler(getType, value, "", "focus", "blur");
+              break;
+            // responsive breakpoints
+            default:
+              this.handleResponsive(getPrefix, getType, value, "");
+          }
+        } else {
+          this.addStyle(getType);
+        }
+        return;
       }
     }
-    // else, apply default / regular style
-    else {
-      this.addStyle(type, value, unit);
+
+    // if not, proceed with the parser
+    const parts = this.parseClassName(className);
+    if (!parts) return;
+    let [parsedPrefix, parsedType, value, unit] = parts;
+
+    // default handler, if the class name has prefix
+    if (parsedPrefix) {
+      switch (parsedPrefix) {
+        case "hover":
+          this.pseudoHandler(parsedType, value, unit, "mouseover", "mouseout");
+          break;
+        case "focus":
+          this.pseudoHandler(parsedType, value, unit, "focus", "blur");
+          break;
+        default:
+          this.handleResponsive(parsedPrefix, parsedType, value, unit);
+      }
+    } else {
+      // apply default style
+      this.addStyle(parsedType, value, unit);
     }
   }
+
   // just applyStyles, but with more confidential :)
   public applyMultiStyles(styles: string): void {
     // splitting the styles and apply each styles with applyStyles method
